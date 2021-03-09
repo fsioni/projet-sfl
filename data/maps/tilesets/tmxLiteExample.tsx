7ad6034dac19e2e58841b5bf9e2a880a60dd105f
SIONI Fares p1907037 <fares.sioni@etu.univ-lyon1.fr>
2021-03-09 10:28:15 +0100

    Ajout tilemap

---
+++ data/maps/tilesets/tmxLiteExample.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="tileset1" tilewidth="48" tileheight="48" tilecount="136" columns="8">
+ <image source="../../textures/tilesets/tmxLiteExample.png" trans="ff00ff" width="384" height="816"/>
+</tileset>
