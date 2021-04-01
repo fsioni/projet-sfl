6185aee27346d29f88a61e753298dab81944499c
SIONI Fares p1907037 <fares.sioni@etu.univ-lyon1.fr>
2021-04-01 16:37:51 +0200

    Documentation

--- src/Core/Map/CollisionBox.h
+++ src/Core/Map/CollisionBox.h
@@ -5,15 +5,43 @@
 
 #include <string>
 
+/*! \class Box 
+*   \brief Classe représentant une boîte de collision dans un monde 2D
+*
+*   Possède des propriété basiques comme la position, dimension, ID. 
+*   Hérite de la classe Box.
+*/
 class CollisionBox: public Box
 {
 private:
-    /* data */
+    /*// === Données membres privées === //*/
+
 public:
+    /*// === Fonctions membres privées === //*/
+
+    /*! \brief Constructeur par défaut 
+    *
+    * Créer une CollisionBox quelconque avec ses données membres à -1.
+    * Ne suffit pas pour utilisation.
+    */
     CollisionBox(/* args */);
+
+    /*! \brief Constructeur depuis balise d'une map .tmx
+    *
+    * \param [in] objectTag : balise contenant les informations de la CollisionBox dans le .tmx.
+    */
     CollisionBox(const std::string& objectTag);
+    
+    /*! \brief Destructeur
+    *
+    * Destructeur de l'objet CollisionBox
+    */
     ~CollisionBox();
 
+    /*! \brief Fonction test de la classe CollisionBox
+    *
+    *   Effectue des tests pour vérifier l'intégrité de la classe CollisionBox et son fonctionnement
+    */
     void Test() const;
 };
 
