ea1aea50b80a4da51d7fd65615bbaab31a041863
Lyes <lyes.chougar@etu.univ-lyon1.fr>
2021-04-13 11:09:18 +0200

    Mise en place des dialogues

--- src/Core/Entity/NPC.cpp
+++ src/Core/Entity/NPC.cpp
@@ -1,24 +1,39 @@
-/*#include "NPC.h"
+#include "NPC.h"
 #include "EntityWithoutHP.h"
+#include <stdlib.h>
 #include <iostream>
 #include <string>
 #include <cstring>
 #include <assert.h>
-#include <random>
+#include <time.h>
 
 using namespace std;
 
 NPC::NPC() : EntityWithoutHP::EntityWithoutHP(){
 
-    const char *DialogTab[9] = {"Wesh mon lossa","s/o roi nouveau rap mondial", "OV chasse les loups dans la ville",
-    "Santa fé","focus comme si j'ai bu 100 cafés", "jfinis la tache en balle","Bezos, envoie la prod jla désosse"};
+    DialogTab[0] = "Wesh mon lossa";
+    DialogTab[1] = "s/o roi nouveau rap mondial";
+    DialogTab[2] = "OV chasse les loups dans la ville";
+    DialogTab[3] = "Santa fé";
+    DialogTab[4] = "focus comme si j'ai bu 100 cafés";
+    DialogTab[5] = "jfinis la tache en balle";
+    DialogTab[6] = "Bezos, envoie la prod jla désosse";
+    DialogTab[7] = "mot8";
+    DialogTab[8] = "mot9";
 
 }
 
 NPC::NPC(float x, float y, string name) : EntityWithoutHP::EntityWithoutHP(x, y, name){
 
-    const char *DialogTab[9] = {"Wesh mon lossa","s/o roi nouveau rap mondial", "OV chasse les loups dans la ville",
-    "Santa fé","focus comme si j'ai bu 100 cafés", "jfinis la tache en balle","Bezos, envoie la prod jla désosse"};
+    DialogTab[0] = "Wesh mon lossa";
+    DialogTab[1] = "s/o roi nouveau rap mondial";
+    DialogTab[2] = "OV chasse les loups dans la ville";
+    DialogTab[3] = "Santa fé";
+    DialogTab[4] = "focus comme si j'ai bu 100 cafés";
+    DialogTab[5] = "jfinis la tache en balle";
+    DialogTab[6] = "Bezos, envoie la prod jla désosse";
+    DialogTab[7] = "mot8";
+    DialogTab[8] = "mot9";
 
 }
 
@@ -28,23 +43,35 @@ NPC::~NPC(){
     y = 0.0;
 }
 
-void NPC::ReadDialog (){
 
-    int lineNumber = rand()%10;
+void NPC::ReadRandDialog (){
 
-    cout<<*DialogTab[lineNumber]<<endl;
+    int n = RandSentenceLine(0, 8);
 
-}
-
-char* NPC::GetDialog (){
-
-    strcpy(actuallyTelling,DialogTab[2]);
-
-    return actuallyTelling;
+    cout<<DialogTab[n]<<endl;
 
 }
 
-*/
+
+string NPC::GetRandDialog (){
+
+    int n = RandSentenceLine(0, 8);
+
+    return DialogTab[n];
+
+}
+
+
+int NPC:: RandSentenceLine (int mini, int maxi){
+
+    int random;
+
+    int plage = maxi - mini + 1;
+
+    for (int i = 0; i < 100; i++) random = (rand() % plage) + mini;
+
+    return random;
+}
 
 
 
