b1608bf4450b57eac5038e7486deb3d3ef72c86f
Lyes <lyes.chougar@etu.univ-lyon1.fr>
2021-04-13 12:36:04 +0200

    Correctifs déclarations

--- src/Core/Entity/NPC.h
+++ src/Core/Entity/NPC.h
@@ -11,10 +11,6 @@ class NPC : public EntityWithoutHP
 {
 public:
 
-    string DialogTab[9];
-
-    char* actuallyTelling;
-
     NPC();
 
     NPC(float x, float y, string name);
@@ -28,6 +24,10 @@ public:
     string GetRandDialog();
 
     int RandSentenceLine(int minimum, int maximum);
+
+protected:
+
+    string DialogTab[9];
 
 };
 
