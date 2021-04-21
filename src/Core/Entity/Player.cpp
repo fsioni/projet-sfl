6287119b9d1929e611abe7dc91534353dc950762
Lyes <lyes.chougar@etu.univ-lyon1.fr>
2021-04-21 14:56:49 +0200

    Correctif typage Max HP

--- src/Core/Entity/Player.cpp
+++ src/Core/Entity/Player.cpp
@@ -6,7 +6,7 @@
 
 Player::Player() : EntityWithHP(){}
 
-Player::Player(float x, float y, string name, unsigned int hp, unsigned int damage,float speed, unsigned int maxHealth)
+Player::Player(float x, float y, string name, unsigned int hp, unsigned int damage,float speed,int maxHP)
 : EntityWithHP::EntityWithHP(x, y, name, hp, damage, speed, maxHP){}
 
 Player::~Player(){
