fa7d03a96171785f56f03031bf549c267ff34019
SIONI Fares p1907037 <fares.sioni@etu.univ-lyon1.fr>
2021-05-07 18:08:17 +0200

    Ajout polices, couleurs et autres textures

--- src/SFML/AssetManager.cpp
+++ src/SFML/AssetManager.cpp
@@ -8,6 +8,10 @@ AssetManager::AssetManager()
     SetShadowTexture("data/textures/characters/Shadow/Shadow 1.png");
     AddTextureEnemy("data/textures/characters/Soldier/Soldier 01-1.png");
     AddTextureNPC("data/textures/characters/Female/Female 01-1.png");
+    SetBackgroundTexture("data/textures/UI/menuBackground.png");
+    SetHeartTexture("./data/textures/UI/heart.png");
+    SetMainFont("data/fonts/BebasNeue-Regular.ttf");
+    setMainTextColor(sf::Color(245, 222, 92));
 }
 
 AssetManager::~AssetManager()
@@ -70,6 +74,20 @@ void AssetManager::SetPlayerTexture(std::string fileTexture)
                   << std::endl;
 }
 
+void AssetManager::SetTileTexture(std::string fileTexture) 
+{
+    std::fstream file;
+    file.open(fileTexture.c_str(), std::fstream::in);
+    if (file)
+    {
+        tileTexture.loadFromFile(fileTexture);
+    }
+    else
+        std::cout << "Echec SetTileTexture( "
+                  << fileTexture << " ) : fichier inexistant"
+                  << std::endl;
+}
+
 void AssetManager::SetShadowTexture(std::string fileTexture)
 {
     std::fstream file;
@@ -83,6 +101,87 @@ void AssetManager::SetShadowTexture(std::string fileTexture)
                   << fileTexture << " ) : fichier inexistant"
                   << std::endl;
 }
+
+void AssetManager::SetBackgroundTexture(std::string fileTexture) 
+{
+    std::fstream file;
+    file.open(fileTexture.c_str(), std::fstream::in);
+    if (file)
+    {
+        textureBackground.loadFromFile(fileTexture);
+    }
+    else
+        std::cout << "Echec SetBackgroundTexture( "
+                  << fileTexture << " ) : fichier inexistant"
+                  << std::endl;
+}
+
+void AssetManager::SetHeartTexture(std::string fileTexture) 
+{
+    std::fstream file;
+    file.open(fileTexture.c_str(), std::fstream::in);
+    if (file)
+    {
+        textureHeart.loadFromFile(fileTexture);
+    }
+    else
+        std::cout << "Echec SetHeartTexture( "
+                  << fileTexture << " ) : fichier inexistant"
+                  << std::endl;
+}
+
+void AssetManager::SetMainFont(std::string fileFont) 
+{
+    std::fstream file;
+    file.open(fileFont.c_str(), std::fstream::in);
+    if (file)
+    {
+        mainFont.loadFromFile(fileFont);
+    }
+    else
+        std::cout << "Echec SetMainFont( "
+                  << fileFont << " ) : fichier inexistant"
+                  << std::endl;
+}
+
+void AssetManager::setMainTextColor(sf::Color color) 
+{
+    textColor = color;
+}
+
+/*void AssetManager::AddMusic(std::string fileMusic) 
+{
+    std::fstream file;
+    file.open(fileMusic.c_str(), std::fstream::in);
+
+    sf::Music music;
+    if (file)
+    {
+        music.openFromFile(fileMusic);
+        musics.push_back(music);
+    }
+    else
+        std::cout << "Echec AddMusic("
+                  << fileMusic << ") : fichier inexistant"
+                  << std::endl;
+}
+
+void AssetManager::AddSoundBuffer(std::string fileBuffer) 
+{
+        std::fstream file;
+    file.open(fileBuffer.c_str(), std::fstream::in);
+
+    sf::SoundBuffer buffer;
+    if (file)
+    {
+        buffer.loadFromFile(fileBuffer);
+        soundBuffers.push_back(buffer);
+    }
+    else
+        std::cout << "Echec AddSoundBuffer("
+                  << fileBuffer << ") : fichier inexistant"
+                  << std::endl;
+}*/
 
 const std::vector<sf::Texture> &AssetManager::GetTextureEnemy() const
 {
@@ -99,7 +198,42 @@ const sf::Texture &AssetManager::GetPlayerTexture() const
     return texturePlayer;
 }
 
+const sf::Texture& AssetManager::GetTileTexture() const
+{
+    return tileTexture;
+}
+
 const sf::Texture &AssetManager::GetShadowTexture() const
 {
     return textureShadow;
+}
+
+const sf::Texture& AssetManager::GetTextureBackground() const
+{
+    return textureBackground;
+}
+
+const sf::Texture& AssetManager::GetTextureHeart() const
+{
+    return textureHeart;
+}
+
+const sf::Font& AssetManager::GetMainFont() const
+{
+    return mainFont;
+}
+
+const sf::Color& AssetManager::GetMainTextColor() const
+{
+    return textColor;
+}
+
+const std::vector<sf::Music>& AssetManager::GetMusics() const
+{
+    return musics;
+}
+
+const std::vector<sf::SoundBuffer>& AssetManager::GetSoundBuffers() const
+{
+    return soundBuffers;
 }
--- src/SFML/AssetManager.h
+++ src/SFML/AssetManager.h
@@ -8,7 +8,7 @@
 
 /*! \class AssetManager
 *   \brief Classe représentant le gestionnaire des données de l'application
-*   (Textures)
+*   (Textures, sons, polices...)
 * 
 */
 class AssetManager
@@ -23,23 +23,56 @@ public:
     void RemoveTextureNPC(int indice);
 
     void SetPlayerTexture(std::string fileTexture);
+    void SetTileTexture(std::string fileTexture);
     void SetShadowTexture(std::string fileTexture);
+    void SetBackgroundTexture(std::string fileTexture);
+    void SetHeartTexture(std::string fileTexture);
+
+    void SetMainFont(std::string fileFont);
+    void setMainTextColor(sf::Color color);
+
+    void AddMusic(std::string fileMusic);
+    void AddSoundBuffer(std::string fileBuffer);
 
     const std::vector<sf::Texture> &GetTextureEnemy() const;
     const std::vector<sf::Texture> &GetTextureNPC() const;
     const sf::Texture &GetPlayerTexture() const;
+    const sf::Texture &GetTileTexture() const;
     const sf::Texture &GetShadowTexture() const;
+    const sf::Texture &GetTextureBackground() const;
+    const sf::Texture &GetTextureHeart() const;
+
+    const sf::Font &GetMainFont() const;
+    const sf::Color &GetMainTextColor() const;
+    const std::vector<sf::Music> &GetMusics() const;
+    const std::vector<sf::SoundBuffer> &GetSoundBuffers() const;
 
 private:
     /*! \brief Tableau vers les textures des ennemis */
     std::vector<sf::Texture> textureEnemy;
     /*! \brief Tableau vers les textures des personnages non joueur */
     std::vector<sf::Texture> textureNPC;
+
     /*! \brief Texture du joueur */
     sf::Texture texturePlayer;
-
+    /*! \brief Texture des tuiles */
+    sf::Texture tileTexture;
     /*! \brief Texture d'ombre */
     sf::Texture textureShadow;
+    /*! \brief Texture du fond d'ecran du SplashScreen et du menu principal */
+    sf::Texture textureBackground;
+    /*! \brief Texture du coeur */
+    sf::Texture textureHeart;
+
+    /*! \brief Police d'écriture principale */
+    sf::Font mainFont;
+    /*! \brief Couleur d'écriture principale */
+    sf::Color textColor;
+    
+    /*! \brief Tableau vbers les musiques du jeu */
+    std::vector<sf::Music> musics;
+    /*! \brief Tableau vers les buffers des sons du jeu */
+    std::vector<sf::SoundBuffer> soundBuffers;
 };
 
 #endif
