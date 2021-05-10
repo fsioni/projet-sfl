228cefc0fa0e5d56c3b601e26819e8f49f11dd92
Lyes <lyes.chougar@etu.univ-lyon1.fr>
2021-05-10 17:23:47 +0200

    ajout const geters

--- src/Core/Map/Map.h
+++ src/Core/Map/Map.h
@@ -11,7 +11,7 @@
 /*! \class Map
 *   \brief Classe stockant la map.
 *
-*   Possède toutes les informations sur la map comme les différents 
+*   Possède toutes les informations sur la map comme les différents
 *   layer ou encore le tileset.
 */
 class Map
@@ -20,36 +20,36 @@ public:
     /*! \brief Constructeur par défaut */
     Map();
 
-    /*! \brief Constructeur prennant en parametre un fichier .tmx et un .tsx. 
+    /*! \brief Constructeur prennant en parametre un fichier .tmx et un .tsx.
     *
     *   \param [in] tmxFile : Chemin vers un fichier .tmx.
     *   \param [in] tsxFile : Chemin vers un fichier .tsx.
-    *   
-    *   Appelle les fonctions tmxLoadLayers et TsxLoadTileset avec 
+    *
+    *   Appelle les fonctions tmxLoadLayers et TsxLoadTileset avec
     *   les fichiers passés en parametre.
     */
     Map(std::string tmxFile, std::string tsxFile);
 
-    /*! \brief Destructeur 
+    /*! \brief Destructeur
     *
     *   Détruit l'objet Map.
     */
     ~Map();
 
     /*! \brief Modifie tileset par celui en paramètre.
-    *   
+    *
     *   \param [in] ts : Tileset à copié dans tileset.
     */
     void SetTileset(const Tileset &ts);
 
     /*! \brief Ajout d'un MapLayer dans le tableau dynamique mapLayers.
-    *   
+    *
     *   \param [in] l : MapLayer à ajouter.
     */
     void AddMapLayer(const MapLayer &l);
 
     /*! \brief Modifie spawnsLayers par celui en paramètre.
-    *   
+    *
     *   \param [in] nLayer : SpawnsLayer à copié dans spawnsLayer
     */
     void SetSpawnLayer(const SpawnsLayer &nLayer);
@@ -64,7 +64,7 @@ public:
     *
     * \return Le collisionLayers de la Map.
     */
-    CollisionLayer *GetCollisionLayer();
+    CollisionLayer *GetCollisionLayer() const;
 
     /*! \brief Retourne mapLayers
     *
@@ -82,7 +82,7 @@ public:
     *
     *   \param [in] fileName : Chemin vers un fichier .tmx.
     *
-    *   Recupère les informations contenu dans le fichier passé 
+    *   Recupère les informations contenu dans le fichier passé
     *   en paramètre et les affecte aux données membres associés.
     */
     void TmxLoadLayers(std::string fileName);
@@ -91,7 +91,7 @@ public:
     *
     *   \param [in] fileName : Chemin vers un fichier .tsx.
     *
-    *   Recupère les informations contenu dans le fichier passé 
+    *   Recupère les informations contenu dans le fichier passé
     *   en paramètre et les affecte aux données membres associés.
     */
     void TsxLoadTileset(std::string fileName);
@@ -108,12 +108,12 @@ private:
     /*! \brief Tableau dynamique de mapLayer, lesdifférentes couches de la map. */
     std::vector<MapLayer> mapLayers;
 
-    /*! \brief  CollisionLayer contenant la couche des objets avec lequel les 
+    /*! \brief  CollisionLayer contenant la couche des objets avec lequel les
     *           entités peuvent entrer en collision.
     */
     CollisionLayer *collisionLayer;
 
-    /*! \brief   SpawnLayer contenant les positions du spawn du joueur et 
+    /*! \brief   SpawnLayer contenant les positions du spawn du joueur et
     *            ceux des ennemis.
     */
     SpawnsLayer *spawnsLayer;
