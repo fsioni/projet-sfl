64b06180d0c28b1d772fee5d8ce02a7c5f801d50
Stanislas Bagnol <stanislas.bagnol@etu.univ-lyon1.fr>
2021-05-10 11:01:53 +0200

    Doc Doxygen

--- src/Core/Entity/FiniteStateMachine/StateMachine.h
+++ src/Core/Entity/FiniteStateMachine/StateMachine.h
@@ -4,21 +4,43 @@
 #include "StateBehavior.h"
 #include "../Player.h"
 
+/*! \class StateMachine<entity_type> 
+*   \brief Modélise un Finite State Machine (FSM) qui permet de
+*          créer des comportement prédéfinit pour des entité.
+*/
 template <class entity_type>
 class StateMachine
 {
 public:
+    /*! \brief Constructeur prenant un pointeur sur entity_type en paramètre.*/
     StateMachine(entity_type *owner_);
+
+    /*! \brief Modifie l'état courrant par celui passé en paramètre.
+    *   \param [in] s : Pointeur sur StateBehavior<entity_type>. 
+    */
     void SetCurrentState(StateBehavior<entity_type> *s);
+
+    /*! \brief Actualise l'état courrant. 
+    *
+    *   \param [in] player_ : std::unique_ptr<Player>, le joueur.
+    *   \param [in] collision : pointeur sur CollisionLayer, permet d'avoir accès
+    *                           à la couche de collision pour les déplacements.
+    *   \param [in] dt : entier positif, deltaTime.
+    */
     void UpdateCurrentState(std::unique_ptr<Player> &player_,
                             CollisionLayer *collision, int dt);
+
+    /*! \brief Permet de changer l'état courrant.
+    *   \param [in] newState : pointeur sur StateBehavior<entity_type>, nouveau currentState.
+    */
     void ChangeState(StateBehavior<entity_type> *newState);
-    void RevertToPreviousState();
+
 
 private:
+    /*! \brief Pointeur sur l'entité du StateMachine. */
     entity_type *owner;
+    /*! \brief Pointeur sur l'état courrant. */
     StateBehavior<entity_type> *currentState;
-    StateBehavior<entity_type> *previousState;
 };
 
 // Definition dans le .h à cause des templates
@@ -28,7 +50,6 @@ StateMachine<entity_type>::StateMachine(entity_type *owner_)
 {
     owner = owner_;
     currentState = nullptr;
-    previousState = nullptr;
 }
 
 template <class entity_type>
@@ -48,16 +69,9 @@ void StateMachine<entity_type>::UpdateCurrentState(
 template <class entity_type>
 void StateMachine<entity_type>::ChangeState(StateBehavior<entity_type> *newState)
 {
-    previousState = currentState;
     currentState->Exit(owner);
     currentState = newState;
     currentState->Enter(owner);
-}
-
-template <class entity_type>
-void StateMachine<entity_type>::RevertToPreviousState()
-{
-    ChangeState(previousState);
 }
 
 #endif
